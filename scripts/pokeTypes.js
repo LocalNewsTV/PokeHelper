09240f6be49639808cbd825f9958b497b12fca75
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2022-08-19 14:53:27 -0700

    Added Desktop CSS

--- scripts/pokeTypes.js
+++ scripts/pokeTypes.js
@@ -1,128 +1,219 @@
 const pokeTyping = {
     bug: {
         color: '#A6B91A',
-        veryEffective: ['Dark', 'Grass', 'Psychic'],
-        notVeryEffective: ['Fighting', 'Fire', 'Flying', 'Ghost', 'Poison', 'Steel'],
-        weakAgainst: ['Fire', 'Flying', 'Rock'],
-        immuneTo: [],
+        offense: {
+            supereffective: ['Dark', 'Grass', 'Psychic'],
+            halfDamage: ['Fighting', 'Fire', 'Flying', 'Ghost', 'Poison', 'Steel'],
+        },
+        defense: {
+            supereffective: ['Fire', 'Flying', 'Rock'],
+            halfDamage: ['Grass', 'Fighting', 'Ground'],
+            immune: [],
+        },
     },
     dark: {
         color: '#705746',
-        veryEffective: ['Ghost', 'Psychic'],
-        notVeryEffective: ['Dark', 'Fairy', 'Fighting'],
-        weakAgainst: ['Bug', 'Fairy', 'Fighting'],
-        immuneTo: ['Psychic'],
+        offense: {
+            supereffective: ['Ghost', 'Psychic'],
+            halfDamage: ['Dark', 'Fairy', 'Fighting'],
+        },
+        defense: {
+            supereffective: ['Bug', 'Fairy', 'Fighting'],
+            halfDamage: ['Dark', 'Ghost'],
+            immune: ['Psychic'],
+        },
     },
     dragon: {
         color: '#6F35FC',
-        veryEffective: ['Dragon'],
-        notVeryEffective: ['Steel'],
-        weakAgainst: ['Dragon', 'Fairy', 'Ice'],
-        immuneTo: [],
-    },
-    ice: {
-        color: '#96D9D6',
-        veryEffective: ['Dragon', 'Flying', 'Grass', 'Ground'],
-        notVeryEffective: ['Fire', 'Ice', 'Steel', 'Water'],
-        weakAgainst: ['Fighting', 'Fire', 'Rock', 'Steel'],
-        immuneTo: [],
+        offense: {
+            supereffective: ['Dragon'],
+            halfDamage: ['Steel'],
+        },
+        defense: {
+            supereffective: ['Dragon', 'Fairy', 'Ice'],
+            halfDamage: ['Fire', 'Water', 'Electric', 'Grass'],
+            immune: [],
+        },
     },
     electric: {
         color: '#F7D02C',
-        veryEffective: ['Flying', 'Water'],
-        notVeryEffective: ['Dragon', 'Electric', 'Grass'],
-        weakAgainst: ['Grass'],
-        immuneTo: [],
+        offense: {
+            supereffective: ['Flying', 'Water'],
+            halfDamage: ['Dragon', 'Electric', 'Grass'],
+        },
+        defense: {
+            supereffective: ['Ground'],
+            halfDamage: ['Electric', 'Flying', 'Steel'],
+            immune: [],
+        },
     },
     fairy: {
         color: '#D685AD',
-        veryEffective: ['Dark', 'Dragon', 'Fighting'],
-        notVeryEffective: ['Dragon', 'Electric', 'Grass'],
-        weakAgainst: ['Poison', 'Steel'],
-        immuneTo: ['Dragon'],
+        offense: {
+            supereffective: ['Dark', 'Dragon', 'Fighting'],
+            halfDamage: ['Dragon', 'Electric', 'Grass'],
+        },
+        defense: {
+            supereffective: ['Poison', 'Steel'],
+            halfDamage: ['Fighting', 'Bug', 'Dark'],
+            immune: ['Dragon'],
+        },
     },
     fighting: {
         color: '#C22E28',
-        veryEffective: ['Dark', 'Ice', 'Normal', 'Rock', 'Steel'],
-        notVeryEffective: ['Bug', 'Fairy', 'Flying', 'Poison', 'Psychic'],
-        weakAgainst: ['Fairy', 'Flying', 'Psychic'],
-        immuneTo: [],
+        offense: {
+            supereffective: ['Dark', 'Ice', 'Normal', 'Rock', 'Steel'],
+            halfDamage: ['Bug', 'Fairy', 'Flying', 'Poison', 'Psychic'],
+        },
+        defense: {
+            supereffective: ['Fairy', 'Flying', 'Psychic'],
+            halfDamage: ['Bug', 'Rock', 'Dark'],
+            immune: [],
+        },
     },
     fire: {
         color: '#EE8130',
-        veryEffective: ['Bug', 'Grass', 'Ice', 'Steel'],
-        notVeryEffective: ['Dragon', 'Fire', 'Rock', 'Water'],
-        weakAgainst: ['Ground', 'Rock', 'Water'],
-        immuneTo: [],
+        offense: {
+            supereffective: ['Bug', 'Grass', 'Ice', 'Steel'],
+            halfDamage: ['Dragon', 'Fire', 'Rock', 'Water'],
+        },
+        defense: {
+            supereffective: ['Ground', 'Rock', 'Water'],
+            halfDamage: ['Fire', 'Grass', 'Ice', 'Bug', 'Steel', 'Fairy'],
+            immune: [],
+        },
     },
     flying: {
         color: '#A98FF3',
-        veryEffective: ['Bug', 'Fighting', 'Grass'],
-        notVeryEffective: ['Electric', 'Rock', 'Steel'],
-        weakAgainst: ['Electric', 'Ice', 'Rock'],
-        immuneTo: ['Ground'],
+        offense: {
+            supereffective: ['Bug', 'Fighting', 'Grass'],
+            halfDamage: ['Electric', 'Rock', 'Steel'],
+        },
+        defense: {
+            supereffective: ['Electric', 'Ice', 'Rock'],
+            halfDamage: ['Grass', 'Fighting', 'Bug'],
+            immune: ['Ground'],
+        },
     },
     ghost: {
         color: '#735797',
-        veryEffective: ['Ghost', 'Psychic'],
-        notVeryEffective: ['Dark'],
-        weakAgainst: ['Dark','Ghost'],
-        immuneTo: ['Normal'],
+        offense: {
+            supereffective: ['Ghost', 'Psychic'],
+            halfDamage: ['Dark'],
+        },
+        defense: {
+            supereffective: ['Dark','Ghost'],
+            halfDamage: ['Poison', 'Bug'],
+            immune: ['Normal', 'Fighting'],
+        },
     },
     grass: {
         color: '#7AC74C',
-        veryEffective: ['Ground', 'Rock', 'Water'],
-        notVeryEffective: ['Bug', 'Fire', 'Flying', 'Grass', 'Poison', 'Steel'],
-        weakAgainst: ['Bug', 'Fire', 'Flying', 'Ice', 'Poison'],
-        immuneTo: [],
+        offense: {
+            supereffective: ['Ground', 'Rock', 'Water'],
+            halfDamage: ['Bug', 'Fire', 'Flying', 'Grass', 'Poison', 'Steel'],
+        },
+        defense: {
+            supereffective: ['Bug', 'Fire', 'Flying', 'Ice', 'Poison'],
+            halfDamage: ['Water', 'Electric', 'Grass', 'Ground'],
+            immune: [],
+        },
     },
     ground: {
         color: '#E2BF65',
-        veryEffective: ['Electric', 'Fire', 'Poison', 'Rock', 'Steel'],
-        notVeryEffective: ['Bug', 'Grass'],
-        weakAgainst: ['Grass', 'Ice', 'Water'],
-        immuneTo: ['Electric'],
+        offense: {
+            supereffective: ['Electric', 'Fire', 'Poison', 'Rock', 'Steel'],
+            halfDamage: ['Bug', 'Grass'],
+        },
+        defense: {
+            supereffective: ['Grass', 'Ice', 'Water'],
+            halfDamage: ['Poison', 'Ground'],
+            immune: ['Electric'],
+        },
+    },
+    ice: {
+        color: '#96D9D6',
+        offense: {
+            supereffective: ['Dragon', 'Flying', 'Grass', 'Ground'],
+            halfDamage: ['Fire', 'Ice', 'Steel', 'Water'],
+        },
+        defense: {
+            supereffective: ['Fighting', 'Fire', 'Rock', 'Steel'],
+            halfDamage: ['Ice'],
+            immune: [],
+        },
+
     },
     normal: {
         color: '#A8A77A',
-        veryEffective: [],
-        notVeryEffective: ['Rock', 'Steel'],
-        weakAgainst: ['Fighting'],
-        immuneTo: [],
+        offense: {
+            supereffective: [],
+            halfDamage: ['Rock', 'Steel'],
+        },
+        defense: {
+            supereffective: ['Fighting'],
+            halfDamage: [],
+            immune: ['Ghost'],
+        },
     },
     poison: {
         color: '#A33EA1',
-        veryEffective: ['Fairy', 'Grass'],
-        notVeryEffective: ['Poison', 'Ghost', 'Ground', 'Rock'],
-        weakAgainst: ['Ground', 'Psychic'],
-        immuneTo: [],
+        offense: {
+            supereffective: ['Fairy', 'Grass'],
+            halfDamage: ['Poison', 'Ghost', 'Ground', 'Rock'],
+        },
+        defense: {
+            supereffective: ['Ground', 'Psychic'],
+            halfDamage: ['Grass', 'Fighting', 'Ghost', 'Bug', 'Fairy'],
+            immune: [],
+        },
     },
     psychic: {
         color: '#F95587',
-        veryEffective: ['Fairy', 'Grass'],
-        notVeryEffective: ['Steel', 'Psychic'],
-        weakAgainst: ['Bug', 'Dark', 'Ghost'],
-        immuneTo: [],
+        offense: {
+            supereffective: ['Fighting', 'Poison'],
+            halfDamage: ['Steel', 'Psychic'],
+        },
+        defense: {
+            supereffective: ['Bug', 'Dark', 'Ghost'],
+            halfDamage: ['Fighting', 'Psychic'],
+            immune: [],
+        },
     },
     rock: {
         color: '#B6A136',
-        veryEffective: ['Bug', 'Fire', 'Flying', 'Ice'],
-        notVeryEffective: ['Fighting', 'Ground', 'Steel'],
-        weakAgainst: ['Fighting', 'Grass', 'Ground', 'Steel', 'Water'],
-        immuneTo: [],
+        offense: {
+            supereffective: ['Bug', 'Fire', 'Flying', 'Ice'],
+            halfDamage: ['Fighting', 'Ground', 'Steel'],
+        },
+        defense: {
+            supereffective: ['Fighting', 'Grass', 'Ground', 'Steel', 'Water'],
+            halfDamage: ['Normal', 'Fire', 'Ghost', 'Flying'],
+            immune: [],
+        },
     },
     steel: {
         color: '#B7B7CE',
-        veryEffective: ['Fairy', 'Ice', 'Rock'],
-        notVeryEffective: ['Electric', 'Fire', 'Steel', 'Water'],
-        weakAgainst: ['Fighting', 'Fire', 'Ground'],
-        immuneTo: ['Poison'],
+        offense: {
+            supereffective: ['Fairy', 'Ice', 'Rock'],
+            halfDamage: ['Electric', 'Fire', 'Steel', 'Water'],
+        },
+        defense: {
+            supereffective: ['Fighting', 'Fire', 'Ground'],
+            halfDamage: ['Normal', 'Grass', 'Ice', 'Flying', 'Psychic', 'Bug', 'Rock', 'Dragon', 'Steel', 'Fairy'],
+            immune: ['Poison'],
+        },
     },
     water: {
         color: '#6390F0',
-        veryEffective: ['Fire', 'Ground', 'Rock'],
-        notVeryEffective: ['Dragon', 'Grass', 'Water'],
-        weakAgainst: ['Electric', 'Grass'],
-        immuneTo: [],
+        offense: {
+            supereffective: ['Fire', 'Ground', 'Rock'],
+            halfDamage: ['Dragon', 'Grass', 'Water'],
+        },
+        defense: {
+            supereffective: ['Electric', 'Grass'],
+            halfDamage: ['Fire', 'Water', 'Ice', 'Steel'],
+            immune: [],
+        },
     },
 }
--- scripts/script.js
+++ scripts/script.js
@@ -2,15 +2,43 @@
 const newE = (ele) => document.createElement(ele);
 const getPokeData = async () => {
     try{
-        const pokemon = (document.getElementById('searchBar').value).toLowerCase(); 
+        const pokemon = ($('#searchBar').val()).toLowerCase();            
+        if(pokemon.valueOf() == new String('shaymin').valueOf()){
+            notAPokeMon('Did you mean Shaymin-land or Shaymin-Sky?')
+            return;
+        }
+        else if(pokemon.valueOf() === new String('').valueOf()){
+            notAPokeMon('Field Cannot be Blank');
+            return;
+        }
         const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
         const responseURL = await fetch(url);
         const response = responseURL.json();
+        if(responseURL.status === 404){
+            notAPokeMon();
+        }
         return response;
     } catch (ex) {
-        console.log(ex);
-        alert("Invalid Pokemon");
+        alert("Invalid response");
     }
+}
+const eliminateStrengths = (pokeTypes, pokeWeaknesses) => {
+    const allStrengths = [];
+    for(const type in pokeTypes){
+            allStrengths.push(...pokeTyping[pokeTypes[type]].defense.halfDamage);
+            allStrengths.push(...pokeTyping[pokeTypes[type]].defense.immune)
+    }
+    const filteredItems = pokeWeaknesses.filter((value) => {
+        if(!allStrengths.includes(value)){
+            return value;
+        }
+    }) 
+    return filteredItems;
+}
+
+const notAPokeMon = (warning = 'Invalid Pokemon Requested') => {
+    $('.weakness, .weakBanner, .imgCont').html('');
+    $('.pokeName').html(warning)
 }
 const whoIsIt = (pokeInfo) => {
     const name = pokeInfo.name[0].toUpperCase() + pokeInfo.name.substring(1);
@@ -23,6 +51,9 @@ const whoIsIt = (pokeInfo) => {
     $(reg).attr('src', pokeInfo.sprites.front_default);
     $('.imgCont').html('');
     $('.imgCont').append(reg, shiny);
+    const pokeType = gatherType(pokeInfo);
+    showData(pokeType, 'pokeTyping');
+    
 }
 const gatherType = (pokeInfo) => {
     const pokeTypes = []; 
@@ -34,33 +65,66 @@ const gatherType = (pokeInfo) => {
 const typeMatching = (pokeTypes) => {
     const pokeWeakness = [];
     pokeTypes.forEach(item => {
-        pokeWeakness.push(...pokeTyping[item].weakAgainst)
+        pokeWeakness.push(...pokeTyping[item].defense.supereffective)
     })
     return [...new Set(pokeWeakness)];
 }
-const showData = (weaknesses) => {
-    $('.weakness').html('');
+const showData = (typesListed, target) => {
+    $(`.${target}`).html('');
     $('.weakBanner').html('They are weak to:')
-    weaknesses.forEach(item => {
+    typesListed.forEach(item => {
         const cont = newE('div');
         $(cont).addClass('typeCard')
         $(cont).css('background-color', pokeTyping[item.toLowerCase()].color);
         $(cont).html(item);
-        $('.weakness').append(cont);
+        $(`.${target}`).append(cont);
     });
+}
+const manualMain = () => {
+    const first = $('#typeOne').val(); 
+    const second = $('#typeTwo').val();
+    const searchTypes = []
+    if(first.valueOf() != new String('none').valueOf()){
+        searchTypes.push(first);
+    }
+    if(second.valueOf() != new String('none').valueOf()){
+        searchTypes.push(second);
+    }
+    const weaknesses = typeMatching(searchTypes)
+    $('.weakness, .pokeName, .pokeTyping, .types, .weakBanner, .imgCont').html('');
+    const finalList = eliminateStrengths(searchTypes, weaknesses)
+    showData(finalList, 'weakness')
 }
 const searchPokeMain = async () => {
     const pokeInfo = await getPokeData();
-    console.log(pokeInfo)
-    whoIsIt(pokeInfo);
-    const pokeTypes = gatherType(pokeInfo);
-    const weaknesses = typeMatching(pokeTypes);
-    showData(weaknesses);
+    if(pokeInfo.valueOf() === new String('no').valueOf()){
+        notAPokeMon()
+    } else {
+        whoIsIt(pokeInfo);
+        const pokeTypes = gatherType(pokeInfo);
+        const weaknesses = typeMatching(pokeTypes);
+        const finalWeakness = eliminateStrengths(pokeTypes, weaknesses)
+        showData(finalWeakness, 'weakness');
+    }
 }
+const fillOptions = () => {
+    const typesList = Object.keys(pokeTyping);
+    for(const types in typesList){
+        const type = newE('option');
+        type.value = typesList[types];
+        $(type).html(typesList[types]);
+        $('#typeOne, #typeTwo').append(type);
+    }
+}
+$(document).ready(() => {
+    fillOptions();
+})
+
 $('#searchBarButt').on('click', searchPokeMain);
 $('#searchBar').on('keypress', (e)=>{
     if(e.key === 'Enter')
         searchPokeMain();
 })
+$('#manualButt').click(manualMain)
 $('#manualMode').toggle();
 $('.toggleType').on('click', ()=>{$('#searchMode, #manualMode').toggle()});
